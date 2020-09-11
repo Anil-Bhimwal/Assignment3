12a5cc8edb0b2748f35b09fa536735eafe82b469
Anil-Bhimwal1 <anil.bhimwal@oyorooms.com>
2020-09-11 16:06:23 +0530

    DishDetail component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import NavbarComponent from './components/Navbar';
 import Menu from './components/MenuComponent';
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <Menu />
-    </div>
-  );
+import { DISHES } from './shared/dishes.js';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    )
+  }
+
 }
+
 
 export default App;
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Card, CardImg, CardBody, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, CardText, CardFooter, CardTitle, Button, CardHeader, Badge } from 'reactstrap';
+
+function renderComments(comments) {
+    if (comments == null) {
+        return (<div></div>);
+    }
+    const cmnts = comments.map(comment => {
+        return (
+            <ListGroupItem key={comment.id}>
+                <ListGroupItemHeading>
+                <Badge>
+                {comment.author},
+                        &nbsp;
+                        {new Intl.DateTimeFormat('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: '2-digit'
+                }).format(new Date(comment.date))}
+                </Badge> 
+                </ListGroupItemHeading>
+                <ListGroupItemText>
+                    {comment.comment}
+                </ListGroupItemText>
+            </ListGroupItem>
+        );
+    })
+    return (
+        <ListGroup>
+            {cmnts}
+        </ListGroup>
+        
+
+    )
+}
+
+
+export default function DishDetail(props) {
+    if (props.dish != null) {
+        let dish = props.dish;
+        return (
+            <div className="row">
+                <Card Body className="col-lg-6 dish-image">
+                    <CardImg top width="100%" src={dish.image} alt={dish.name} />
+
+                </Card>
+                <Card Body className="col-lg-6">
+                    <CardHeader className="text-right">
+                        <button>Add</button>
+                    </CardHeader>
+                    <CardBody>
+                        <CardTitle>
+                            <h3>
+                                {dish.name}
+                            </h3>
+                        </CardTitle>
+                        <cardtext>{dish.description}</cardtext>
+                        <h4 className="comments-header">
+                            Comments:
+                        </h4>
+                        <cardtext>
+                            {renderComments(dish.comments)}
+                        </cardtext>
+
+                    </CardBody>
+                </Card>
+            </div>
+        );
+
+    } else {
+        return (
+            <div></div>
+        )
+    }
+}
