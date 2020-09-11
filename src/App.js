329a1778903bf42e20d7ef3b4b2f9ec61a3eeda6
Anil-Bhimwal1 <anil.bhimwal@oyorooms.com>
2020-09-11 13:56:41 +0530

    first MenuComponent added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import NavbarComponent from './components/Navbar';
-
+import Menu from './components/MenuComponent';
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <Menu />
     </div>
   );
 }
