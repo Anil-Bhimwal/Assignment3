03ee7d3107f6f37d2428ad1e6243438c8fce77d2
Anil-Bhimwal1 <anil.bhimwal@oyorooms.com>
2020-09-14 15:28:24 +0530

    Login modal is added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
 import NavbarComponent from './Navbar';
-import {Jumbotron } from 'reactstrap';
+import {
+  Jumbotron, Button, Modal, ModalHeader, ModalBody,
+  Form, FormGroup, Input, Label
+} from 'reactstrap';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false
+    }
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: " + this.password.value
+        + " Remember: " + this.remember.checked);
+    event.preventDefault();
+
+}
   render() {
-    return(
-    <React.Fragment>
-      <NavbarComponent/>
-      <Jumbotron>
-           <div className="container">
-               <div className="row row-header">
-                   <div className="col-12 col-sm-6">
-                       <h1>Continuous Food</h1>
-                       <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
-                   </div>
-               </div>
-           </div>
-       </Jumbotron>
-    </React.Fragment>
+    return (
+      <React.Fragment>
+        <NavbarComponent toggleModal={this.toggleModal} />
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Continuous Food</h1>
+                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input type="text" id="username" name="username"
+                  innerRef={(input) => this.username = input} />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" name="password"
+                  innerRef={(input) => this.password = input} />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" name="remember"
+                    innerRef={(input) => this.remember = input} />
+                                    Remember me
+                                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">Login</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </React.Fragment>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,66 +1,70 @@
 import React, { useState } from 'react';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem, Button
 } from 'reactstrap';
 
 const NavbarComponent = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+    const toggle = () => setIsOpen(!isOpen);
 
-  return (
-    <div>
-      <Navbar  style={{backgroundColor:'#41444a' ,color: '#ffffff'}} variant="dark"light expand="md">
-        <NavbarBrand style={{color: '#ffffff', fontStyle:"italic"}}href="/">
-            <img src="assets/images/logo.png" height="40" width="50" alt="Continuous Food" />
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink style={{color: '#ffffff'}} href="/home">Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{color: '#ffffff'}} href="/menu">Menu</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{color: '#ffffff'}} href="/about">About</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{color: '#ffffff'}} href="/contact">Contact Us</NavLink>
-            </NavItem>
-            <UncontrolledDropdown style={{backgroundColor:'#41444a'}} nav inNavbar>
-              <DropdownToggle style={{backgroundColor:'#41444a' ,color: '#ffffff'}} nav caret>
-                Categories
+    return (
+        <div>
+            <Navbar style={{ backgroundColor: '#41444a', color: '#ffffff' }} variant="dark" light expand="md">
+                <NavbarBrand style={{ color: '#ffffff', fontStyle: "italic" }} href="/">
+                    <img src="assets/images/logo.png" height="40" width="100" alt="Continuous Food" />
+                </NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink style={{ color: '#ffffff' }} href="/home">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink style={{ color: '#ffffff' }} href="/menu">Menu</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink style={{ color: '#ffffff' }} href="/about">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink style={{ color: '#ffffff' }} href="/contact">Contact Us</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown style={{ backgroundColor: '#41444a' }} nav inNavbar>
+                            <DropdownToggle style={{ backgroundColor: '#41444a', color: '#ffffff' }} nav caret>
+                                Categories
               </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem >
-                  Beverages
+                            <DropdownMenu right>
+                                <DropdownItem >
+                                    Beverages
                 </DropdownItem>
-                <DropdownItem > 
-                  Lunch Items
+                                <DropdownItem >
+                                    Lunch Items
                 </DropdownItem>
-                <DropdownItem >
-                  Dinner
+                                <DropdownItem >
+                                    Dinner
                 </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <Button outline onClick={props.toggleModal} style={{ color: '#ffffff', marginTop:"5px" }}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
 }
 
 export default NavbarComponent;
