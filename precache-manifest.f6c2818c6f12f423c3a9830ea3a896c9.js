aa3c2f20f3cc894531f50d7d444a365929f443b5
Anil-Bhimwal1 <anil.bhimwal@oyorooms.com>
2020-09-16 13:20:27 +0530

    Updates

---
+++ precache-manifest.f6c2818c6f12f423c3a9830ea3a896c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a30e0d695e6120bd98a6e79a0d41e12",
+    "url": "/Assignment3/index.html"
+  },
+  {
+    "revision": "e4a1a6b5d7c8628eace5",
+    "url": "/Assignment3/static/css/2.8ce0910b.chunk.css"
+  },
+  {
+    "revision": "3bdbb5f6b68668e287f1",
+    "url": "/Assignment3/static/css/main.33b7aa5f.chunk.css"
+  },
+  {
+    "revision": "e4a1a6b5d7c8628eace5",
+    "url": "/Assignment3/static/js/2.9f2438a8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Assignment3/static/js/2.9f2438a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bdbb5f6b68668e287f1",
+    "url": "/Assignment3/static/js/main.41c6f9b2.chunk.js"
+  },
+  {
+    "revision": "0f9d18de0beccfaae8df",
+    "url": "/Assignment3/static/js/runtime-main.30e2e242.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Assignment3/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Assignment3/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Assignment3/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Assignment3/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Assignment3/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
